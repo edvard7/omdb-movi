9cea9ebd722f31cbc674524724920e46971bd883
Кадровский Сергей <279014109@TOP.ACADEMY>
2023-12-28 21:17:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import Movies from './components/Movies';
+import { useState, useEffect } from 'react';
+import { APIKEY } from './env.js';
+
+import './App.scss';
 
 function App() {
-  const URL = "https://reactjs.org"
+  const [movies, setMovies] = useState ([  ]); 
+  const [searchField, setSearchField] = useState('');
+  const URL = `https://www.omdbapi.com/?s=${searchField}&apikey=${APIKEY}`
+
+  //   {
+  //   Title: "Star Wars: Episode IV - A New Hope",
+  //   Year: "1977",
+  //   Poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+  // }
+
+  // async function fetchMovies(){
+  //   const data = await fetch(URL);
+  //   const movies = data.json();
+  //   setMovies(movies.Search);
+  // }
+
+  function getMovies(){
+    // fetchMovies()
+    fetch(encodeURI(URL))
+    .then (res => res.json())
+    .then (movies => setMovies(movies.Search))
+  }
+
+  useEffect(getMovies, []);
+  useEffect(getMovies, [searchField]);
+
+  let timer;
+  function handleChange(event){
+  clearTimeout(timer);
+
+    setTimeout(() => {
+    setSearchField(event.target.value)
+  }, 3000)
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href={URL}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleChange} />
+      <Movies movies={movies} />
     </div>
   );
 }
+
 
 export default App;
